e71bc608ea4fb04dacff7217d2bfae08a58fdb3b
abdallahelghobary <abdualahghobary@gmail.com>
2026-02-07 20:05:53 +0200

    commit

---
+++ android/app/src/main/kotlin/com/example/start_application1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.start_application1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BurthDayApp());
+}
+
+class BurthDayApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Birthday App',
+      home: Scaffold(
+       
+            
+        body: Image(image:  AssetImage('images/a_dot_burr.jpeg' ) ,
+        
+        
+        
+         ),
+
+          
+        
+       
+      ),
+      
+    );
+  }
+}   
